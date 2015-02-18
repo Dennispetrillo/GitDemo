c23188fb6df5b15ee5ea9a83ebb73f1839471abc
Dennis P <dpetrillo@rallydev.com>
2015-02-18 12:51:39 -0500

    S38 a few more changes

--- App.js
+++ App.js
@@ -3,6 +3,6 @@ Ext.define('CustomApp', {
     componentCls: 'app',
     items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc3/doc/">App SDK 2.0rc3 Docs</a>'},
     launch: function() {
-        //add some comments to the app
+        //another change
     }
 });
